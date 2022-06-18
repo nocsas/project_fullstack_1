d89cbfaf7e3528d6e5ca1be11ee5e70db0b1d0a1
nocsas <dwisasongkomukti@gmail.com>
2022-06-18 13:44:18 +0700

    frontend_1

---
+++ frontend/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <ItemNavbar/>
+    <div class="container pt-4">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script> 
+  import ItemNavbar from '@/components/ItemNavbar.vue'
+  
+  export default { 
+    components: {
+      ItemNavbar
+    }
+  }
+</script>
+<style>
+  #app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; 
+  color: #2c3e50;
+  } 
+  #app .navbar-nav a{ 
+  color: #2c3e50;
+  cursor: pointer;
+  }
+  #app .navbar-nav a.router-link-exact-active {
+  color: #42b983;
+  }
+</style>
